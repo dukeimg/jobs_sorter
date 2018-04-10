e7cf0fc68aaf51efd3d2fdcef09f577126071b72
Viktor Danilov <duke.img@gmail.com>
2018-04-10 19:12:03 +0300

    Simple implementation

--- jobs_sorter.rb
+++ jobs_sorter.rb
@@ -1,7 +1,25 @@
+require 'json'
+require 'byebug'
+
 module JobsSorter
   module_function
 
   def run(input)
-    input
+    return [] if input.empty?
+    structure = JSON.parse(input)
+
+    [].tap do |arr|
+      structure.each do |job, dependency|
+        next if arr.index(job)
+        next arr.push(job) if dependency.nil?
+        arr.push(*resolve_dependency(job, structure), job)
+      end
+    end.uniq
+  end
+
+  def resolve_dependency(job, jobs)
+    dependency = jobs.fetch(job)
+    return [] if dependency.nil?
+    [resolve_dependency(dependency, jobs), dependency].flatten
   end
 end
