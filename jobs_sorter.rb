b29a750dc5d7018e9db6e50a6123011d10ef51ea
Viktor Danilov <duke.img@gmail.com>
2018-04-10 19:25:46 +0300

    Handle bad dependencies

--- jobs_sorter.rb
+++ jobs_sorter.rb
@@ -10,16 +10,33 @@ module JobsSorter
 
     [].tap do |arr|
       structure.each do |job, dependency|
-        next if arr.index(job)
+        next if arr.include?(job)
         next arr.push(job) if dependency.nil?
-        arr.push(*resolve_dependency(job, structure), job)
+        validate_dependency(job, dependency)
+        arr.push(*resolve_dependencies(job, structure), job)
       end
     end.uniq
   end
 
-  def resolve_dependency(job, jobs)
+  def resolve_dependencies(job, jobs)
     dependency = jobs.fetch(job)
     return [] if dependency.nil?
-    [resolve_dependency(dependency, jobs), dependency].flatten
+    [].tap do |chain|
+      loop do
+        validate_dependency_chain(chain)
+        dependency = jobs.fetch(job)
+        break if dependency.nil?
+        chain.push(dependency)
+        job = dependency
+      end
+    end.reverse
+  end
+
+  def validate_dependency(job, dependency)
+    raise ArgumentError, "Jobs can't depend on themselves" if job == dependency
+  end
+
+  def validate_dependency_chain(chain)
+    raise ArgumentError, "Jobs dependencies can't loop"  if chain.uniq != chain
   end
 end
